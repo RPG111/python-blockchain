1c6f2bcd85befa08b57760703bb2f7553ac350fe
MrHackIT <rakesh127001@gmail.com>
2020-04-14 14:26:17 +0530

    added mine_block

--- block.py
+++ block.py
@@ -1,17 +1,45 @@
+import time
+
+def mine_block(last_block, data):
+    """
+    Mine a block based on the given last_block and data.
+    """
+    timestamp = time.time_ns()
+    last_hash = last_block.hash
+    hash = f'{timestamp}-{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+    """
+    Generates the genesis block.
+    """
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+
 class Block:
     """
     Block: a unit of storage.
     Store transcations in a blockchain that supports a cyrptocurrency.
     """
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
     def __repr__(self):
-        return f'Block - Data : {self.data}'
-
+        return (
+            'Block('
+            f'timestamp: {self.timestamp},'
+            f'last_hash: {self.last_hash},'
+            f'hash: {self.hash},'
+            f'data: {self.data})'
+        )
+        
 def main():
-    block = Block('foo')
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 if __name__ == '__main__':
     main()
