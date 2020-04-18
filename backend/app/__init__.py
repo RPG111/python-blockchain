3e46a40207bed64bc9310895d05c23e0c05d452d
MrHackIT <rakesh127001@gmail.com>
2020-04-18 18:33:02 +0530

    Synchronize a Peer on Startup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import random
 
 from flask import Flask, jsonify
@@ -28,10 +29,19 @@ def route_blockchain_mine():
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
     return jsonify(block.to_json())
-
-PORT = 5000
+ROOT_PORT = 5000
+PORT = ROOT_PORT
 
 if os.environ.get('PEER') == 'True':
     PORT = random.randint(5001, 6000)
+
+    result = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
+    result_blockchain = Blockchain.from_json(result.json())
+
+    try:
+        blockchain.replace_chain(result_blockchain.chain)
+        print(f'\n -- Successfully synchronized the local chain')
+    except Exception as e:
+        print(f'\n -- Error synchronizing: {e}')
 
 app.run(port=PORT)
--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -40,6 +40,20 @@ class Blockchain:
         return list(map(lambda block: block.to_json(), self.chain))
 
     @staticmethod
+    def from_json(chain_json):
+        """
+        Deserialize a list of serialized blocks into a Blockchain instance.
+        The result will contain a chain list of Block instances.
+        """
+        blockchain = Blockchain()
+        blockchain.chain = list(
+            map(lambda block_json: Block.from_json(block_json), chain_json)
+        )
+
+        return blockchain
+
+
+    @staticmethod
     def is_valid_chain(chain):
         """
         Validate the incoming chain.
