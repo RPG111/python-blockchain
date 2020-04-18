39a3bde60b5551f69b730b757c00d1de2dffb50b
MrHackIT <rakesh127001@gmail.com>
2020-04-18 15:37:20 +0530

    blockchain_data_on_flask

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 
+from backend.blockchain.blockchain import Blockchain
+
 app = Flask(__name__)
+blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(i)
 
 @app.route('/')
-def default():
+def route_default():
     return 'Welcome to the blockchain'
+
+@app.route('/blockchain')
+def route_blockchain():
+    return blockchain.__repr__()
 
 app.run()
